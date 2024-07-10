a0aa9ce57af4545c7b3acb038fb813393a623a9a
nicktsan <nicktsang.manhay@gmail.com>
2024-07-09 22:31:19 -0700

    Created the initial create deck page. Form in progress

---
+++ app/(primary)/createdeck/page.tsx
@@ -0,0 +1,9 @@
+export default async function CreateDeck() {
+
+    return (
+      <div className="flex-1 w-full flex flex-col gap-20 items-center">
+        this is the create deck page
+      </div>
+    );
+  }
+  
